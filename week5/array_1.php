96d8436e25e2be1a8514ccbcc07fa514ea44d902
Aqillaprly <aqillaaprly.gmail.com>
2024-10-01 05:10:37 +0700

    2

--- week5/array_1.php
+++ week5/array_1.php
@@ -5,11 +5,12 @@
     <body>
     <h2>Array Terindeks</h2>
     <?php
-        $Listdosen=["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+        $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
 
-        echo $Listdosen[2] . "<br>";
-        echo $Listdosen[0] . "<br>";
-        echo $Listdosen[1] . "<br>";
+        // Using a for loop to display the array elements
+        for ($i = 0; $i < count($Listdosen); $i++) {
+            echo $Listdosen[$i] . "<br>";
+        }
     ?>
     </body>
-    </html>
+</html>
