a9c1c007621656a8083d5caf0fd0a626cddecc9a
Aqillaprly <aqillaaprly.gmail.com>
2024-10-02 09:19:34 +0700

    no4

--- week5/array_2.php
+++ week5/array_2.php
@@ -4,7 +4,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Document</title>
+    <style>
+        table {
+            width: 50%;
+            border-collapse: collapse;
+            margin: 20px 0;
+            font-family: Arial, sans-serif;
+            font-size: 16px;
+        }
+
+        th,
+        td {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        tr:hover {
+            background-color: #f1f1f1;
+        }
+    </style>
 </head>
 
 <body>
@@ -12,12 +36,28 @@
     $Dosen = [
         'nama' => 'Elok Nur Hamdana',
         'domisili' => 'Malang',
-        'jenis_kelamin' => 'Perempuan'];
-    echo "Nama :{$Dosen['nama']} <br>";
-    echo "Domisili :{$Dosen['domisili']} <br>";
-    echo "Jenis Kelamin :{$Dosen['jenis_kelamin']} <br>";
+        'jenis_kelamin' => 'Perempuan'
+    ];
     ?>
 
+    <table>
+        <tr>
+            <th>Field</th>
+            <th>Details</th>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td><?php echo $Dosen['nama']; ?></td>
+        </tr>
+        <tr>
+            <td>Domisili</td>
+            <td><?php echo $Dosen['domisili']; ?></td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+        </tr>
+    </table>
 </body>
 
 </html>
---
+++ week5/function.php
@@ -0,0 +1,9 @@
+<?php
+
+function perkenalan(){
+    echo "Assalamualikum, ";
+    echo "Perkenalkan, nama saya Aqilla <br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+perkenalan();
+?>
