eccbbd55aeab798d99a1df0f5a8fffd310aa421e
Aqillaprly <aqillaaprly.gmail.com>
2024-10-02 20:52:08 +0700

    9

--- week5/function_return.php
+++ week5/function_return.php
@@ -1,10 +1,19 @@
 <?php
-//membuat fungsi 
-function hitunganUmur($thn_lahir, $thn_sekarang){
+//membuat fungsi
+function hitungumur($thn_lahir, $thn_sekarang)
+{
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
+function perkenalan($nama, $salam = "Assalammualaikum")
+{
+    echo $salam . ",";
+    echo "Perkenalan, nama saya " . $nama . "<br/>";
 
-echo " Umur saya adalah " . hitunganUmur(2004, 2024) ."tahun"
+    //memanggil fungsi lain
+    echo "Saya berusia " . hitungumur(1988, 2023) . " tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
 
-?>
+//memanggil fungsi perkenalan
+perkenalan("Elok");
