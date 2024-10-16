b290670d1a93324a7cd3be3956f1be8e6e438955
Aqillaprly <aqillaaprly.gmail.com>
2024-10-16 21:22:18 +0700

    7

---
+++ week7/form1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Input</title>
+</head>
+<body>
+    <h2>Form Input</h2>
+    <form method="post" action="html_safe.php">
+        <label for="input">Enter something: </label>
+        <input type="text" name="input" id="input" required><br><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+</body>
+</html>
