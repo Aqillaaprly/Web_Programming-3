bc32e734c76a22072f9c81b661b06cbe125401ac
Aqillaprly <aqillaaprly.gmail.com>
2024-10-16 23:40:24 +0700

    12

--- week7/form1.php
+++ week7/form1.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Form Input</title>
+<title>HTML Form</title>
 </head>
 <body>
-    <h2>Form Input</h2>
-    <form method="post" action="html_safe.php">
-        <label for="input">Enter something: </label>
-        <input type="text" name="input" id="input" required><br><br>
-        <input type="submit" name="submit" value="Submit">
-    </form>
+
+<form method="post" action="html_safe.php">
+<label for="email">Email:</label>
+<input type="text" name="email" id="email"><br><br>
+<input type="submit" value="Submit">
+</form>
+
 </body>
 </html>
--- week7/html_safe.php
+++ week7/html_safe.php
@@ -1,4 +1,9 @@
 <?php
-    $input = $_POST['input'];
-    $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+$email = $_POST['email'];
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "Email valid: " . $email;
+} else {
+    echo "Email tidak valid";
+}
 ?>
